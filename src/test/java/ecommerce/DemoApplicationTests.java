4708b904ceef8ad5c4319bec5ae45b096df1f333
saeed-moradi-98 <moradis1@myumanitoba.ca>
2025-11-24 22:07:01 -0600

    Initial commit: Product Service microservice setup

---
+++ src/test/java/ecommerce/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package full_stack_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
